d1f081be1dd4dac67d97ab3efcd6afc091a14413
Silveira-DEEV <eduardoantoniosilveira071@gmail.com>
2025-06-29 20:39:33 -0300

    Rename gitignore

--- obj/Debug/net9.0/WebApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b1387584559b8160c3ada4d599efa72c11824b2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b4cfb868b2fbd8a0c8062f73526ab5a66dbabe")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
